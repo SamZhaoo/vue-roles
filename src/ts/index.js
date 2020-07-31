da2922183386820355435fe2ee3498a2fe0d2229
zhaoakun <zhaoakun@qq.com>
2020-07-31 17:34:05 +0800

    .

---
+++ src/ts/index.js
@@ -0,0 +1,4 @@
+var age = 18;
+var stature = 178.5;
+console.log(age);
+console.log(stature);
---
+++ src/ts/index.ts
@@ -0,0 +1,4 @@
+var age:number = 18
+var stature:number = 178.5
+console.log(age)
+console.log(stature)
